04d0cf06ac7192598f2a1404f76dc18eb96f16cf
Emmanuel Adiba <eabaagah@gmail.com>
2021-08-21 19:40:13 +0000

    setup routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
+import { BrowserRouter, Route, Link } from 'react-router-dom'
+import StreamCreate from './components/streams/StreamCreate';
+import StreamEdit from './components/streams/StreamEdit';
+import StreamDelete from './components/streams/StreamDelete';
+import StreamList from './components/streams/StreamList';
+import StreamShow from './components/streams/StreamShow';
+
 function App() {
-  return (
-    <div>
-      <h2>App</h2>
-    </div>
-  );
+    return (
+      <div>
+      <BrowserRouter>
+        <div>
+          <Route path="/" exact component={StreamList} />
+          <Route path="/streams/new" exact component={StreamCreate} />
+          <Route path="/streams/edit" exact component={StreamEdit} />
+          <Route path="/streams/delete" exact component={StreamDelete} />
+          <Route path="/streams/show" exact component={StreamShow} />
+        </div>
+      </BrowserRouter>
+      </div>
+    );
 }
 
 export default App;
