ca8b0939dbe9fb12699fb010813ca031fb101d60
Emmanuel Adiba <eabaagah@gmail.com>
2021-08-30 09:27:38 +0000

    create a parent form component

--- src/components/streams/StreamEdit.jsx
+++ src/components/streams/StreamEdit.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { getStream } from '../../actions'
+export class StreamEdit extends Component {
 
-const StreamEdit = () => {
-    return (
-        <div>
-            StreamEdit
-        </div>
-    )
+    componentDidMount() {
+        this.props.getStream(this.props.match.params.id)
+    }
+
+    render() {
+        console.log(this.props);
+
+        if(!this.props.stream) {
+            return <div>No Stream</div>
+        }
+        return (
+            <div>
+                {this.props.stream.title}
+            </div>
+        )
+    }
 }
 
-export default StreamEdit
+const mapStateToProps = (state, ownProps) => {
+    return { stream: state.streams[ownProps.match.params.id]}
+}
+
+export default connect(mapStateToProps, { getStream })(StreamEdit)
