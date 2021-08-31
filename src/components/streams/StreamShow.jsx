383a96ed338990882494ea499574b475400fd166
Emmanuel Adiba <eabaagah@gmail.com>
2021-08-31 22:39:42 +0000

    add video element to StreamShow

--- src/components/streams/StreamShow.jsx
+++ src/components/streams/StreamShow.jsx
@@ -1,11 +1,38 @@
-import React from 'react'
+import React, { Component } from 'react'
+import flv from 'flv.js'
+import { connect } from 'react-redux'
+import { getStream } from '../../actions'
+export class StreamShow extends Component {
 
-const StreamShow = () => {
-    return (
-        <div>
-            StreamShow
-        </div>
-    )
+    constructor(props) {
+        super(props)
+        this.videoRef = React.createRef()
+    }
+    componentDidMount() {
+        this.props.getStream(this.props.match.params.id)
+    }
+
+    
+    render() {
+        
+        if(!this.props.stream) {
+            return <div>Loading...</div>
+        }
+
+        const { title, description } = this.props.stream
+
+        return (
+            <div>
+            <video ref={this.videoRef} style={{ width: '100%'}} controls />
+                <h1>{title}</h1>
+                <h5>{description}</h5>
+            </div>
+        )
+    }
 }
 
-export default StreamShow
+const mapStateToProps = (state, ownProps) => {
+    return { stream: state.streams[ownProps.match.params.id] }
+}
+
+export default connect(mapStateToProps, { getStream })(StreamShow)
